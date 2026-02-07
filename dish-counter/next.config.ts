758c60a24afaa23f0495665675338c432edb87d3
krishna123 <krishnatulaskar786.com>
2026-02-07 15:50:22 +0530

    Fix : Updated next.config.js with the necessary changes.

--- dish-counter/next.config.ts
+++ dish-counter/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enable static export for Firebase Hosting
   reactCompiler: true,
+  distDir: 'out',    // Optional: Specify the output directory for static files
 };
 
 export default nextConfig;
